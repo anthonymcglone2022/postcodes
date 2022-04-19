792ede4022e3f8045bcbcc22f7952b1862334da7
anthonymcglone2022 <anthonymcglone2022@gmail.com>
2022-04-19 21:47:58 +0100

    Removing dummy sql file

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO post_code (code, inuse, district) VALUES("AB1 0AA","No","Aberdeen City"),
-("AB1 0AB","No","Aberdeen City"),
-("AB1 0AD","No","Aberdeen City"),
-("AB1 0AE","No","Aberdeenshire"),
-("AB1 0AF","No","Aberdeen City"),
-("AB1 0AG","No","Aberdeen City"),
-("AB1 0AJ","No","Aberdeen City");
