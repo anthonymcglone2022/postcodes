f464d14f8e523aa0fa354e7f0ad25962ba4d75a5
anthonymcglone2022 <anthonymcglone2022@gmail.com>
2022-04-28 22:27:50 +0100

    Initial attempt at Integration tests

---
+++ src/test/java/com/uk/postcodes/PostCodeTests.java
@@ -0,0 +1,34 @@
+package com.uk.postcodes;
+
+import org.json.JSONObject;
+import org.springframework.http.MediaType;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
+
+import com.uk.postcodes.entity.PostCode;
+import com.uk.postcodes.controller.PostCodeController;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class PostCodeTests {
+
+	@Autowired
+	PostCodeController postCodeController;
+
+	@Test
+	void createAPostCode() {
+		PostCode postCode = new PostCode();
+		postCode.setCode("W5 1AT");
+		postCode.setInuse("Yes");
+        	postCode.setDistrict("Ealing");
+
+		Object returnedPostCode = postCodeController.addPostCode(postCode);
+	        Assertions.assertThat(returnedPostCode).isNotNull();
+	}
+}
